1668b5724df4c5cbd047ac2d03a575f4b7445b78
fast-facts <107660271+fast-facts@users.noreply.github.com>
2022-06-22 19:57:48 -0400

    chore: Move from node-run-cmd to execa

--- src/update-version.ts
+++ src/update-version.ts
@@ -1,11 +1,11 @@
+import * as execa from 'execa';
 import { readFileSync } from 'fs';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const cmd = require('node-run-cmd');
+void (async () => {
+  const packageJson = JSON.parse(readFileSync('./node_modules/imgur/package.json').toString());
+  const version = (packageJson.version as string).replace('^', '');
 
-const packageJson = JSON.parse(readFileSync('./node_modules/imgur/package.json').toString());
-const version = (packageJson.version as string).replace('^', '');
+  console.log(`Imgur version ${version}`);
 
-console.log(`Imgur version ${version}`);
-
-cmd.run(`sed -i s/0.0.0/${version}/g dist/package.json`, { shell: true });
+  await execa(`sed -i s/0.0.0/${version}/g dist/package.json`);
+})();
